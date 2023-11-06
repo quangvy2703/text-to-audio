0503426ee64145f9f8417a3c3542140f3a4a8464
vypq <vypq@rever.vn>
2023-11-06 22:07:30 +0700

    update

--- train.sh
+++ train.sh
@@ -9,8 +9,8 @@
 
 # Continue training the LDM from our checkpoint using the --hf_model argument
 accelerate launch train.py \
---train_file="data/train_audiocaps.json" --validation_file="data/valid_audiocaps.json" --test_file="data/test_audiocaps_subset.json" \
---hf_model "declare-lab/tango" --unet_model_config="configs/diffusion_model_config.json" --freeze_text_encoder \
+--train_file="data/zalo_converted/train.json" --validation_file="data/zalo_converted/valid.json" \
+--hf_model "declare-lab/tango-full-ft-audio-music-caps" --unet_model_config="configs/diffusion_model_config.json" --freeze_text_encoder \
 --gradient_accumulation_steps 4 --per_device_train_batch_size=2 --per_device_eval_batch_size=2 --augment \
 --learning_rate=3e-5 --num_train_epochs 40 --snr_gamma 5 \
 --text_column captions --audio_column location --checkpointing_steps="best"
